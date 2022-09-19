8214efb0f6b2587c69b158e4b2f68b6179dacd6f
mimi <kuni2097@gmail.com>
2022-09-19 20:17:41 +0900

    getStringExtra 입력

--- app/src/main/java/com/miminerica/intentpractice/MessageActivity.kt
+++ app/src/main/java/com/miminerica/intentpractice/MessageActivity.kt
@@ -1,14 +1,17 @@
 package com.miminerica.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
 
-        finish()
+        val getMsg = intent.getStringExtra("msg")
+        messageTxt.text = getMsg
 
     }
 }
