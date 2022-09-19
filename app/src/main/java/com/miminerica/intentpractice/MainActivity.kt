a444b738d71fe868b005cd01721eefc488107054
mimi <kuni2097@gmail.com>
2022-09-19 19:20:45 +0900

    MainActivity 작성

--- app/src/main/java/com/miminerica/intentpractice/MainActivity.kt
+++ app/src/main/java/com/miminerica/intentpractice/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.miminerica.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainBtn.setOnClickListener {
+
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
+
 }
